2816c0cd74c09dc6ad503a12e140ce0bd264e68d
autodiag2 <autodiag@netcourrier.com>
2025-02-05 11:21:59 +0100

    add emulator commands

--- dist/windows/virtual_serial_port_tools_simulate_elm327.ts
+++ dist/windows/virtual_serial_port_tools_simulate_elm327.ts
@@ -20,33 +20,61 @@ class LoopbackSerialDevicePull implements Port.IScriptDevice
     	var length = s.length;
     	var index = -1;
     	var hex;
-	var arr = [];
+		var arr = [];
     	while (++index < length) {
     	    arr.push(s.charCodeAt(index));
     	}
     	return Uint8Array.from(arr);
     }
-    async run()
-    {
-	const SERIAL_OK = "OK";
-	var eol =  "\r\n";
-	log("Started emulator");
-        while (true)
-        {
-            	var sentData = await port.getSentData();	
-		const receivedString = String.fromCharCode(...sentData).toLowerCase();
-		log(receivedString);
-
-	    	var response = receivedString;
-		if ( receivedString == ("ate0" + eol) ) {
-			response = SERIAL_OK + eol;
-		} else if ( receivedString == ("ati" + eol) ) {
-			response = "ELM327 v1.5" + eol;
-		} else if ( receivedString == ("atl0" + eol) ) {
-			response = SERIAL_OK + eol;
-			eol = "\r";
+	getStateDefault() {
+		return {
+			echo: true,
+			eol: "\r\n",
+			baud_rate: 38400,
 		}
-            	port.provideReceivedData(this.stringToUIntArray(response));
+	}
+    async run() {
+		const SERIAL_OK = "OK";
+		let elm327 = this.getStateDefault();
+		const commands = {
+			echo: /ate([\d])/,
+			linefeeds: /atl([\d])/,
+		};
+		log("Started emulator");
+        while (true) {
+            var sentData = await port.getSentData();	
+			const receivedString = String.fromCharCode(...sentData).toLowerCase();
+			log(receivedString);
+
+	    	let response;
+			if ( commands.echo.exec(receivedString) != null ) {
+				elm327.echo = Boolean(parseInt(commands.echo.exec(receivedString)[1]));
+				response = SERIAL_OK;
+			} else if (receivedString == ("atib 10" + elm327.eol)) {
+				elm327.baud_rate = 10400;
+				response = SERIAL_OK;
+			} else if ( receivedString == ("ati" + elm327.eol) ) {
+				response = "ELM327 v2.1";
+			} else if ( receivedString == ("atign" + elm327.eol) ) {
+				response = Math.random() < 0.5 ? "ON" : "OFF";
+			} else if ( receivedString == ("atd" + elm327.eol) ) {
+				elm327 = this.getStateDefault();
+				response = SERIAL_OK;
+			} else if ( commands.linefeeds.exec(receivedString) != null ) {
+				if ( Boolean(parseInt(commands.linefeeds.exec(receivedString)[1])) ) {
+					elm327.eol = "\r\n";
+				} else {
+					elm327.eol = "\r";
+				}
+				response = SERIAL_OK;
+			} else {
+				response = receivedString;
+			}
+			if ( elm327.echo ) {
+				response = receivedString + response;
+			}
+			response += elm327.eol;
+            port.provideReceivedData(this.stringToUIntArray(response));
         }
     }
 }
