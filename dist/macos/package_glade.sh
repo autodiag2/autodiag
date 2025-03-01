9d87b4e884c6bbb24e600e3ad935bb16655bd403
ffprivacy <ffprivacy@netcourrier.com>
2025-03-01 11:08:55 +0100

    fix

--- dist/macos/package_glade.sh
+++ dist/macos/package_glade.sh
@@ -1,11 +1,22 @@
 APP_NAME="glade"
 APP_PATH="./bin/$APP_NAME.app"
 APP_PACKAGE="./bin/$APP_NAME.dmg"
+FRAMEWORKS_PATH="$APP_PATH/Contents/Frameworks"
 
 EXECS_PATH="$APP_PATH/Contents/MacOS/"
+EXECUTABLE="$EXECS_PATH/glade"
 
-mkdir -p "$EXECS_PATH"
+. ./dist/macos/package_lib.sh
+
+mkdir -p "$EXECS_PATH" "$FRAMEWORKS_PATH"
+
 cp /opt/homebrew/bin/glade "$EXECS_PATH"
+process_dependencies "$EXECUTABLE" "$FRAMEWORKS_PATH"
+
+# Process all libraries inside Frameworks folder recursively
+for LIB in "$FRAMEWORKS_PATH"/*.dylib; do
+    process_dependencies "$LIB" "$FRAMEWORKS_PATH"
+done
 
 # Ad-hoc sign the app
 codesign --force --deep --sign - "$APP_PATH"
