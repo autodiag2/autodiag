ab2c17d001fe4cb7fd2d6d36ba80203592ca797d
ffprivacy <ffprivacy@netcourrier.com>
2025-06-09 19:50:34 +0200

    improve example

--- pyautodiag/pyautodiag/examples/serial_open_send_recv.py
+++ pyautodiag/pyautodiag/examples/serial_open_send_recv.py
@@ -1,12 +1,19 @@
+import sys
 from pyautodiag.libautodiag.com.serial import Serial
+from pyautodiag.libautodiag.log import log_set_level, LOG_DEBUG, LOG_NONE
+
+if len(sys.argv) < 2:
+    print("Error: Please provide the port location as an argument.")
+    sys.exit(1)
+
+port_location = sys.argv[1]
 
 serial = Serial()
-serial.name = "/dev/ttys030".encode('utf-8')
+serial.name = port_location.encode('utf-8')
 serial.dump()
 serial.open()
 serial.send("ATZ")
 serial.recv()
-from pyautodiag.libautodiag.log import log_set_level, LOG_DEBUG, LOG_NONE
 log_set_level(LOG_DEBUG)
 serial.recv_buffer.contents.dump()
 log_set_level(LOG_NONE)
