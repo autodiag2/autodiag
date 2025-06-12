f2f31657aa2dea369fa8560ba897f8de5861536b
ffprivacy <ffprivacy@netcourrier.com>
2025-06-12 19:48:37 +0200

    mutate example

--- pyautodiag/autodiag/examples/elm_complete.py
+++ pyautodiag/autodiag/examples/elm_complete.py
@@ -2,6 +2,7 @@ import sys
 from autodiag.com.serial import Serial
 from autodiag.com.serial.elm.elm import ELMDevice
 from autodiag.log import log_set_level, LOG_DEBUG, LOG_NONE
+
 from ctypes import cast, POINTER
 
 if len(sys.argv) < 2:
@@ -13,15 +14,25 @@ port_location = sys.argv[1]
 log_set_level(LOG_DEBUG)
 serial = Serial()
 serial.name = port_location.encode('utf-8')
-serial.debug()
-serial.debug_from_python()
+#serial.debug()
+#serial.debug_from_python()
 iface = ELMDevice.open_from_serial(serial)
-print(iface.device)
+#print(iface.device)
 elm = cast(iface.device, POINTER(ELMDevice)).contents
-elm.debug()
-elm.debug_from_python()
+#elm.debug()
+#elm.debug_from_python()
+iface.vehicle.contents.debug()
+iface.vehicle.contents.debug_from_python()
+iface.clear_data()
+#elm.debug()
+#elm.debug_from_python()
+iface.vehicle.contents.debug()
+iface.vehicle.contents.debug_from_python()
 iface.send("0101")
 iface.recv()
+iface.vehicle.contents.debug()
+iface.vehicle.contents.debug_from_python()
 iface.send("ati")
 iface.recv()
+iface.vehicle.contents.dump()
 iface.close()
