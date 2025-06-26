f1478f3561dedc18360246ba2fed8e929675eb5c
ffprivacy <ffprivacy@netcourrier.com>
2025-06-26 12:39:45 +0200

    encapse

--- src/main/libautodiag/sim/elm327/ecu_generator_cycle.c
+++ src/main/libautodiag/sim/elm327/ecu_generator_cycle.c
@@ -1,22 +1,22 @@
 #include "libautodiag/sim/elm327/sim_generators.h"
 #include "libautodiag/com/serial/elm/elm327/elm327.h"
 
-int sim_ecu_generator_cycle_percent[0xFF][0xFF] = {0};
-void sim_ecu_generator_cycle_iterate(int service_id, int pid, unsigned gears) {
-    sim_ecu_generator_cycle_percent[service_id][pid] += (100/gears);
-    sim_ecu_generator_cycle_percent[service_id][pid] %= 100;
+static int cycle_percent[0xFF][0xFF] = {0};
+static void cycle_iterate(int service_id, int pid, unsigned gears) {
+    cycle_percent[service_id][pid] += (100/gears);
+    cycle_percent[service_id][pid] %= 100;
 }
-void sim_ecu_generator_response_cycle(SimECUGenerator *generator, char ** response, final Buffer *binResponse, final Buffer *binRequest) {
+static void response(SimECUGenerator *generator, char ** response, final Buffer *binResponse, final Buffer *binRequest) {
     unsigned gears = 10;
     if ( generator->context != null ) {
         gears = *((unsigned*)generator->context);
     }
     switch(binRequest->buffer[0]) {
         case 0x02: case 0x01: {
-            buffer_append(binResponse,buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 2, sim_ecu_generator_cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+            buffer_append(binResponse,buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 2, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
         } break;
         case 0x07: case 0x0A: case 0x03: {
-            buffer_append(binResponse,buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 1, sim_ecu_generator_cycle_percent[binRequest->buffer[0]][0]));                
+            buffer_append(binResponse,buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 1, cycle_percent[binRequest->buffer[0]][0]));                
         } break;
         case 0x04: {
             (*response) = strdup(SerialResponseStr[SERIAL_RESPONSE_OK-SerialResponseOffset]);
@@ -33,7 +33,7 @@ void sim_ecu_generator_response_cycle(SimECUGenerator *generator, char ** respon
                         break;
                     }
                     case 0x02: {
-                        buffer_append(binResponse,buffer_new_cycle(17, sim_ecu_generator_cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));                
+                        buffer_append(binResponse,buffer_new_cycle(17, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));                
                         break;
                     }
                     case 0x03: {
@@ -41,7 +41,7 @@ void sim_ecu_generator_response_cycle(SimECUGenerator *generator, char ** respon
                         break;
                     }
                     case 0x04: {
-                        buffer_append(binResponse,buffer_new_cycle(16, sim_ecu_generator_cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));                
+                        buffer_append(binResponse,buffer_new_cycle(16, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));                
                         break;
                     }
                     case 0x05: {
@@ -49,7 +49,7 @@ void sim_ecu_generator_response_cycle(SimECUGenerator *generator, char ** respon
                         break;
                     }
                     case 0x06: {
-                        buffer_append(binResponse,buffer_new_cycle(4, sim_ecu_generator_cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+                        buffer_append(binResponse,buffer_new_cycle(4, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                     case 0x07: {
@@ -57,7 +57,7 @@ void sim_ecu_generator_response_cycle(SimECUGenerator *generator, char ** respon
                         break;
                     }
                     case 0x08: {
-                        buffer_append(binResponse,buffer_new_cycle(4, sim_ecu_generator_cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+                        buffer_append(binResponse,buffer_new_cycle(4, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                     case 0x09: {
@@ -71,7 +71,7 @@ void sim_ecu_generator_response_cycle(SimECUGenerator *generator, char ** respon
                         break;
                     }
                     case 0x0B: {
-                        buffer_append(binResponse,buffer_new_cycle(4, sim_ecu_generator_cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+                        buffer_append(binResponse,buffer_new_cycle(4, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                 }
@@ -80,12 +80,12 @@ void sim_ecu_generator_response_cycle(SimECUGenerator *generator, char ** respon
             }
         } break;
     }
-    sim_ecu_generator_cycle_iterate(binRequest->buffer[0], 1 < binRequest->size ? binRequest->buffer[1] : 0, gears);
+    cycle_iterate(binRequest->buffer[0], 1 < binRequest->size ? binRequest->buffer[1] : 0, gears);
 }
 
 SimECUGenerator* sim_ecu_generator_new_cycle() {
     SimECUGenerator * generator = sim_ecu_generator_new();
-    generator->response = SIM_ECU_GENERATOR_RESPONSE_FUNC(sim_ecu_generator_response_cycle);
+    generator->response = SIM_ECU_GENERATOR_RESPONSE_FUNC(response);
     generator->type = strdup("cycle");
     return generator;
 }
