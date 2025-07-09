1b3d63f92cacba2ce217fcf1fb57ebd99d64edaf
ffprivacy <ffprivacy@netcourrier.com>
2025-07-09 19:58:04 +0200

    add nrcs

--- src/main/libautodiag/com/uds/uds.c
+++ src/main/libautodiag/com/uds/uds.c
@@ -2,6 +2,62 @@
 
 static object_hashmap_Int_string * service_to_string_map = null;
 static object_hashmap_Int_Int * service_to_response_map = null;
+static object_hashmap_Int_string * nrc_to_string_map = null;
+
+char * uds_nrc_to_string(final UDS_NRC nrc) {
+    if ( nrc_to_string_map == null ) {
+        nrc_to_string_map = object_hashmap_Int_string_new();
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_GENERAL_REJECT), object_string_new_from("General reject"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SERVICE_NOT_SUPPORTED), object_string_new_from("Service not supported"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SUBFUNCTION_NOT_SUPPORTED), object_string_new_from("Sub-function not supported"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_INVALID_MESSAGE_LENGTH), object_string_new_from("Invalid message length/format"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_RESPONSE_TOO_LONG), object_string_new_from("Response too long"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_BUSY_REPEAT_REQUEST), object_string_new_from("Busy-repeat request"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_CONDITIONS_NOT_CORRECT), object_string_new_from("Conditions not correct"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_REQUEST_SEQUENCE_ERROR), object_string_new_from("Request sequence error"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_NO_RESPONSE_FROM_SUBNET), object_string_new_from("No response from subnet component"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_FAILURE_PREVENTS_EXECUTION), object_string_new_from("Failure prevents execution of requested action"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_REQUEST_OUT_OF_RANGE), object_string_new_from("Request out of range"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SECURITY_ACCESS_DENIED), object_string_new_from("Security access denied"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_INVALID_KEY), object_string_new_from("Invalid key"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_EXCEEDED_NUMBER_OF_ATTEMPTS), object_string_new_from("Exceeded number of attempts"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_REQUIRED_TIME_DELAY_NOT_EXPIRED), object_string_new_from("Required time delay has not expired"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_UPLOAD_DOWNLOAD_NOT_ACCEPTED), object_string_new_from("Upload/download not accepted"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_TRANSFER_DATA_SUSPENDED), object_string_new_from("Transfer data suspended"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_PROGRAMMING_FAILURE), object_string_new_from("Programming failure"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_WRONG_BLOCK_SEQUENCE_COUNTER), object_string_new_from("Wrong block sequence counter"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_REQUEST_RECEIVED_RESPONSE_PENDING), object_string_new_from("Request received - response pending"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SUBFUNCTION_NOT_SUPPORTED_IN_SESSION), object_string_new_from("Sub function not supported in active session"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SERVICE_NOT_SUPPORTED_IN_SESSION), object_string_new_from("Service not supported in active session"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_RPM_TOO_HIGH), object_string_new_from("RPM too high"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_RPM_TOO_LOW), object_string_new_from("RPM too low"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_ENGINE_RUNNING), object_string_new_from("Engine is running"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_ENGINE_NOT_RUNNING), object_string_new_from("Engine is not running"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_ENGINE_RUN_TIME_TOO_LOW), object_string_new_from("Engine run time too low"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_TEMPERATURE_TOO_HIGH), object_string_new_from("Temperature too high"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_TEMPERATURE_TOO_LOW), object_string_new_from("Temperature too low"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SPEED_TOO_HIGH), object_string_new_from("Speed too high"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SPEED_TOO_LOW), object_string_new_from("Speed too low"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_THROTTLE_PEDAL_TOO_HIGH), object_string_new_from("Throttle pedal too high"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_THROTTLE_PEDAL_TOO_LOW), object_string_new_from("Throttle pedal too low"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_TRANSMISSION_RANGE_NOT_IN_NEUTRAL), object_string_new_from("Transmission range not in neutral"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_TRANSMISSION_RANGE_NOT_IN_DEAR), object_string_new_from("Transmission range not in dear"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_BRAKE_SWITCHES_NOT_CLOSED), object_string_new_from("Brake switches not closed"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_SHIFTER_LEVEL_NOT_IN_PARK), object_string_new_from("Shifter level not in park"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_TORK_CONVERTER_CLUTCH_LOCKED), object_string_new_from("Tork converter clutch locked"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_VOLTAGE_TOO_HIGH), object_string_new_from("Voltage too high"));
+        object_hashmap_Int_string_set(nrc_to_string_map, object_Int_new_from(UDS_NRC_VOLTAGE_TOO_LOW), object_string_new_from("Voltage too low"));
+    }
+    if ( 0xF0 <= nrc && nrc <= 0xFE ) {
+        return strdup("Manufacturer specific conditions are not met");
+    }
+    object_string * result = object_hashmap_Int_string_get(nrc_to_string_map, object_Int_new_from(nrc));
+    if ( result == null ) {
+        return null;
+    } else {
+        return result->data;
+    }
+}
 
 int uds_service_response(final UDSService key) {
     if ( service_to_response_map == null ) {
