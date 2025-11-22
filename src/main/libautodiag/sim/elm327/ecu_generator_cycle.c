76ff763ce2dbb80153acff324cd9abd5e1bc70a9
ffprivacy <ffprivacy@netcourrier.com>
2025-11-22 11:01:54 +0100

    improve readability

--- src/main/libautodiag/sim/elm327/ecu_generator_cycle.c
+++ src/main/libautodiag/sim/elm327/ecu_generator_cycle.c
@@ -6,23 +6,35 @@ static void cycle_iterate(int service_id, int pid, unsigned gears) {
     cycle_percent[service_id][pid] += (100/gears);
     cycle_percent[service_id][pid] %= 100;
 }
+
 static void response(SimECUGenerator *generator, char ** response, final Buffer *binResponse, final Buffer *binRequest) {
     unsigned gears = 10;
     if ( generator->context != null ) {
         gears = *((unsigned*)generator->context);
     }
+
     switch(binRequest->buffer[0]) {
-        case 0x02: case 0x01: {
-            buffer_append(binResponse,buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 2, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+        case OBD_SERVICE_SHOW_FREEEZE_FRAME_DATA:
+        case OBD_SERVICE_SHOW_CURRENT_DATA: {
+            buffer_append(binResponse,
+                buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 2,
+                cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
         } break;
-        case 0x07: case 0x0A: case 0x03: {
-            buffer_append(binResponse,buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 1, cycle_percent[binRequest->buffer[0]][0]));                
+
+        case OBD_SERVICE_PENDING_DTC:
+        case OBD_SERVICE_PERMANENT_DTC:
+        case OBD_SERVICE_SHOW_DTC: {
+            buffer_append(binResponse,
+                buffer_new_cycle(ISO_15765_SINGLE_FRAME_DATA_BYTES - 1,
+                cycle_percent[binRequest->buffer[0]][0]));
         } break;
-        case 0x04: {
+
+        case OBD_SERVICE_CLEAR_DTC: {
             (*response) = strdup(SerialResponseStr[SERIAL_RESPONSE_OK-SerialResponseOffset]);
         } break;
-        case 0x09: {
-            if ( 1 < binRequest->size ) {            
+
+        case OBD_SERVICE_REQUEST_VEHICLE_INFORMATION: {
+            if ( 1 < binRequest->size ) {
                 switch(binRequest->buffer[1]) {
                     case 0x00: {
                         buffer_append(binResponse, buffer_from_ascii_hex("FFFFFFFF"));
@@ -33,7 +45,9 @@ static void response(SimECUGenerator *generator, char ** response, final Buffer 
                         break;
                     }
                     case 0x02: {
-                        buffer_append(binResponse,buffer_new_cycle(17, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));                
+                        buffer_append(binResponse,
+                            buffer_new_cycle(17,
+                            cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                     case 0x03: {
@@ -41,7 +55,9 @@ static void response(SimECUGenerator *generator, char ** response, final Buffer 
                         break;
                     }
                     case 0x04: {
-                        buffer_append(binResponse,buffer_new_cycle(16, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));                
+                        buffer_append(binResponse,
+                            buffer_new_cycle(16,
+                            cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                     case 0x05: {
@@ -49,7 +65,9 @@ static void response(SimECUGenerator *generator, char ** response, final Buffer 
                         break;
                     }
                     case 0x06: {
-                        buffer_append(binResponse,buffer_new_cycle(4, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+                        buffer_append(binResponse,
+                            buffer_new_cycle(4,
+                            cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                     case 0x07: {
@@ -57,7 +75,9 @@ static void response(SimECUGenerator *generator, char ** response, final Buffer 
                         break;
                     }
                     case 0x08: {
-                        buffer_append(binResponse,buffer_new_cycle(4, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+                        buffer_append(binResponse,
+                            buffer_new_cycle(4,
+                            cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                     case 0x09: {
@@ -71,7 +91,9 @@ static void response(SimECUGenerator *generator, char ** response, final Buffer 
                         break;
                     }
                     case 0x0B: {
-                        buffer_append(binResponse,buffer_new_cycle(4, cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
+                        buffer_append(binResponse,
+                            buffer_new_cycle(4,
+                            cycle_percent[binRequest->buffer[0]][binRequest->buffer[1]]));
                         break;
                     }
                 }
@@ -80,7 +102,10 @@ static void response(SimECUGenerator *generator, char ** response, final Buffer 
             }
         } break;
     }
-    cycle_iterate(binRequest->buffer[0], 1 < binRequest->size ? binRequest->buffer[1] : 0, gears);
+
+    cycle_iterate(binRequest->buffer[0],
+        1 < binRequest->size ? binRequest->buffer[1] : 0,
+        gears);
 }
 
 SimECUGenerator* sim_ecu_generator_new_cycle() {
