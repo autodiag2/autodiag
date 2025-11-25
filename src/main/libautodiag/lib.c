ab5e9a0fcd0ece2e62e8b7fd92c1f32bc58a0830
ffprivacy <ffprivacy@netcourrier.com>
2025-11-25 14:05:39 +0100

    improving the escaper

--- src/main/libautodiag/lib.c
+++ src/main/libautodiag/lib.c
@@ -6,15 +6,31 @@ GENERATE_TYPE_DUP(byte)
 
 char * ascii_escape_breaking_chars(char *str) {
     size_t len = strlen(str);
-    char *res = malloc(len * 2 + 1);
+    char *res = malloc(len * 4 + 1);
     char *p = res;
 
     for (size_t i = 0; i < len; i++) {
-        if (str[i] == '\r') {
+        unsigned char c = (unsigned char)str[i];
+
+        if (c == '\\') {
+            *p++ = '\\';
+            *p++ = '\\';
+        } else if (c == '\r') {
             *p++ = '\\';
             *p++ = 'r';
+        } else if (c == '\n') {
+            *p++ = '\\';
+            *p++ = 'n';
+        } else if (c == '\t') {
+            *p++ = '\\';
+            *p++ = 't';
+        } else if (c < 0x20 || c > 0x7E) {
+            *p++ = '\\';
+            *p++ = 'x';
+            sprintf(p, "%02X", c);
+            p += 2;
         } else {
-            *p++ = str[i];
+            *p++ = c;
         }
     }
 
