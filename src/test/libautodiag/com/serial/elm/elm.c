3c94e2dee1482083fedcbc9dcab6fec16e022ac9
ffprivacy <ffprivacy@netcourrier.com>
2025-12-08 11:36:24 +0100

    implement the breaking change

--- src/test/libautodiag/com/serial/elm/elm.c
+++ src/test/libautodiag/com/serial/elm/elm.c
@@ -25,5 +25,26 @@ bool testElm(final VehicleIFace* iface) {
             if ( bin->buffer[i] != i ) return false;
         }
     }
+    {
+        SimELM327* elm327 = sim_elm327_new();
+        LIST_SIM_ECU(elm327->ecus)[0].list[0]->generator = sim_ecu_generator_new_citroen_c5_x7();
+        sim_elm327_loop_as_daemon(elm327);
+        sim_elm327_loop_daemon_wait_ready(elm327);
+        final VehicleIFace* iface2 = port_open(strdup(elm327->device_location));
+        ELM327Device * device = (ELM327Device*)iface2->device;
+        if ( device->protocol < ELM327_PROTO_ISO_15765_4_CAN_1 ) {
+            device->send(device, "attp 6");
+            device->clear_data(device);
+            device->recv(device);
+        } else {
+            device->send(device, "attp 2");
+            device->clear_data(device);
+            device->recv(device);
+        }
+        viface_send_str(iface2, "0100");
+        viface_clear_data(iface2);
+        viface_recv(iface2);
+        assert(0 < iface2->vehicle->data_buffer->size);
+    }
     return true;
 }
