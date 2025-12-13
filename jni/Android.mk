cca50f49f694df4e9c29bc24e5ed5db3d463eaa4
ffprivacy <ffprivacy@netcourrier.com>
2025-12-13 13:54:18 +0100

    refactor

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,19 +1,17 @@
 LOCAL_PATH := $(call my-dir)
 ROOT := $(LOCAL_PATH)/../
-
-include $(ROOT)/app.mk
-include $(CLEAR_VARS)
-
 rwildcard = $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2)) $(wildcard $1$2)
 
+include $(ROOT)/app.mk
+
+CFLAGS = -fms-extensions -Wno-microsoft-anon-tag -Wno-unused-command-line-argument
+CFLAGS += -DCOMPILE_COMPAT
+CFLAGS += -DAPP_NAME="\"$(APP_NAME)\"" -DAPP_VERSION="\"$(APP_VERSION)\""
+CFLAGS += -DAPP_MAINTAINER="\"$(APP_MAINTAINER)\"" -DAPP_DESC="\"$(APP_DESC)\""
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := libautodiag
 LOCAL_C_INCLUDES := $(ROOT)/include/main/ $(ROOT)/cJSON/
-SRC_LIB := ../cJSON/cJSON.c $(patsubst $(LOCAL_PATH)/%,%,$(call rwildcard,$(ROOT)/src/main/libautodiag/,*.c))
-LOCAL_SRC_FILES := $(SRC_LIB)
-
-LOCAL_CFLAGS += -fms-extensions -Wno-microsoft-anon-tag -Wno-unused-command-line-argument
-LOCAL_CFLAGS += -DCOMPILE_COMPAT
-LOCAL_CFLAGS += -DAPP_NAME="\"$(APP_NAME)\"" -DAPP_VERSION="\"$(APP_VERSION)\""
-LOCAL_CFLAGS += -DAPP_MAINTAINER="\"$(APP_MAINTAINER)\"" -DAPP_DESC="\"$(APP_DESC)\""
-
+LOCAL_SRC_FILES := ../cJSON/cJSON.c $(patsubst $(LOCAL_PATH)/%,%,$(call rwildcard,$(ROOT)/src/main/libautodiag/,*.c))
+LOCAL_CFLAGS := $(CFLAGS)
 include $(BUILD_SHARED_LIBRARY)
