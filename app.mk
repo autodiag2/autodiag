329eba020c0f1321d37a5ea86d133b6ecbaff7a0
ffprivacy <ffprivacy@netcourrier.com>
2026-01-01 09:44:42 +0100

    update doc

--- app.mk
+++ app.mk
@@ -14,4 +14,4 @@ Features:\n\
   - Serial console \n\
 Supported devices: ELM320 ELM322 ELM323 ELM327 ELM329 \n\
 Standards: ISO3779 SAEJ1979/ISO15031-5 ISO15031-6 ISO3780 ISO3166-1 ISO15765 ISO14229 OBD2 UDS CAN CAN-TP KWP2000 \n\
-Communication: Bluetooth USB
+Communication: Bluetooth USB Wifi
