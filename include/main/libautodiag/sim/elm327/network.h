26332248e58cff0749b1d568ff257dbe15205107
autodiag2 <autodiag@netcourrier.com>
2026-02-15 19:44:38 +0100

    remove useless

--- include/main/libautodiag/sim/elm327/network.h
+++ include/main/libautodiag/sim/elm327/network.h
@@ -2,23 +2,6 @@
 #define __SIM_ELM327_NETWORK_H
 
 #include "libautodiag/lib.h"
-
-#if defined OS_POSIX
-#   include <unistd.h>
-#   include <errno.h>
-#   include <string.h>
-#   include <arpa/inet.h>
-#   include <netinet/in.h>
-#   include <sys/socket.h>
-    typedef int sock_t;
-#   include <unistd.h>
-#   define close_sock close
-#elif defined OS_WINDOWS
-#   pragma comment(lib, "Ws2_32.lib")
-    typedef SOCKET sock_t;
-#   define close_sock closesocket
-#endif
-
 #include "libautodiag/com/network.h"
 
 #define ELM327_NETWORK_PORT 35000
