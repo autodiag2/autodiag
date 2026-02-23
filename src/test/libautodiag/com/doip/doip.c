df4b9d8ae2a05e9894855878f61e9683965a1c5e
autodiag2 <autodiag@netcourrier.com>
2026-02-23 17:48:47 +0100

    improve tests

--- src/test/libautodiag/com/doip/doip.c
+++ src/test/libautodiag/com/doip/doip.c
@@ -2,9 +2,11 @@
 #include "libautodiag/com/doip/doip.h"
 
 bool testDOIP() {
+    Buffer * target_ecu = buffer_from_ascii_hex("07E8");
+    Buffer * tester_address = buffer_from_ascii_hex("1234");
     {
         Buffer * payload = buffer_from_ascii_hex("1000");
-        object_DoIPMessage * msg = doip_message_diag(buffer_from_ascii_hex("07E8"), buffer_from_ascii_hex("0000"), payload);
+        object_DoIPMessage * msg = doip_message_diag(target_ecu, tester_address, payload);
         Buffer * serialized = doip_message_serialize(msg);
         assert(serialized != null);
         object_DoIPMessage * msg2 = doip_message_parse(serialized);
@@ -19,7 +21,7 @@ bool testDOIP() {
         device->location = location;
         device->open(device);
         Buffer * payload = buffer_from_ascii_hex("1000");
-        object_DoIPMessage * msg = doip_message_diag(buffer_from_ascii_hex("07E8"), buffer_from_ascii_hex("0000"), payload);
+        object_DoIPMessage * msg = doip_message_diag(target_ecu, tester_address, payload);
         doip_message_dump(msg);
         Buffer * serialized = doip_message_serialize(msg);
         char * serialized_str = buffer_to_hex_string(serialized);
