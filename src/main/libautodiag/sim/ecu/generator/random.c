bd55807d13ed2385ac44be1f38aa301da3b0a13c
autodiag2 <autodiag@netcourrier.com>
2026-04-01 13:58:22 +0200

    more accurate vehicle information request

--- src/main/libautodiag/sim/ecu/generator/random.c
+++ src/main/libautodiag/sim/ecu/generator/random.c
@@ -25,6 +25,131 @@ static Buffer * response_saej1979_pid(SimECUGenerator *generator, final byte pid
     }
     return binResponse;
 }
+static Buffer * response_saej1979_vehicle_identification_request(SimECUGenerator * generator, Buffer * binRequest) {
+    Buffer * payload = ad_buffer_new();
+    int infoType_i = 1;
+    byte infoType = binRequest->buffer[infoType_i];
+
+    do {
+        infoType = binRequest->buffer[infoType_i];
+        switch(infoType) {
+            case 0x00: {
+                ad_buffer_append_byte(payload, infoType);
+                ad_buffer_append_melt(payload, ad_buffer_from_ascii_hex("FFFFFFFF"));
+            } break;
+            default: {
+                log_warn("multiple info type not implemented for info type %02X", infoType);
+            } break;
+        }
+        infoType_i++;
+    } while(generator->flavour.is_Iso15765_4 && infoType_i < binRequest->size);
+
+    switch(infoType) {
+        case 0x01: {
+            if ( ! generator->flavour.is_Iso15765_4 ) {
+                ad_buffer_append_byte(payload, infoType);
+                // should not hardcode the vin message count here
+                ad_buffer_append_byte(payload, 0x05);
+            }
+        } break;
+        case OBD_SERVICE_REQUEST_VEHICLE_INFORMATION_VIN: {
+            ad_buffer_append_byte(payload, infoType);
+            if ( generator->flavour.is_Iso15765_4 ) {
+                int number_of_data_items = 1;
+                ad_buffer_append_byte(payload, number_of_data_items);                
+            }
+            ad_buffer_append_melt(payload,
+                ad_buffer_new_random_with_seed(17, seed));
+        } break;
+        case 0x03: {
+            if ( ! generator->flavour.is_Iso15765_4 ) {
+                ad_buffer_append_byte(payload, infoType);
+                // should not hardcode the message count here
+                ad_buffer_append_byte(payload, 0x01);
+            }
+        } break;
+        case 0x04: {
+            ad_buffer_append_byte(payload, infoType);
+            if ( generator->flavour.is_Iso15765_4 ) {
+                int number_of_data_items = 1;
+                ad_buffer_append_byte(payload, number_of_data_items);                
+            }
+            ad_buffer_append_melt(binResponse,
+                ad_buffer_new_random_with_seed(16, seed));
+        } break;
+        case 0x05: {
+            if ( ! generator->flavour.is_Iso15765_4 ) {
+                ad_buffer_append_byte(payload, infoType);
+                // should not hardcode the message count here
+                ad_buffer_append_byte(payload, 0x01);
+            }
+        } break;
+        case 0x06: {
+            ad_buffer_append_byte(payload, infoType);
+            if ( generator->flavour.is_Iso15765_4 ) {
+                int number_of_data_items = 1;
+                ad_buffer_append_byte(payload, number_of_data_items);                
+            }
+            ad_buffer_append_melt(payload,
+                ad_buffer_new_random_with_seed(4, seed));
+            break;
+        }
+        case 0x07: {
+            if ( ! generator->flavour.is_Iso15765_4 ) {
+                ad_buffer_append_byte(payload, infoType);
+                // should not hardcode the message count here
+                ad_buffer_append_byte(payload, 0x01);
+            }
+        } break;
+        case 0x08: {
+            ad_buffer_append_byte(payload, infoType);
+            if ( generator->flavour.is_Iso15765_4 ) {
+                int number_of_data_items = 1;
+                ad_buffer_append_byte(payload, number_of_data_items);                
+            }
+            ad_buffer_append_melt(payload,
+                ad_buffer_new_random_with_seed(4, seed));
+        } break;
+        case 0x09: {
+            if ( ! generator->flavour.is_Iso15765_4 ) {
+                ad_buffer_append_byte(payload, infoType);
+                // should not hardcode the message count here
+                ad_buffer_append_byte(payload, 0x01);
+            }
+        } break;
+        case OBD_SERVICE_REQUEST_VEHICLE_INFORMATION_ECU_NAME: {
+            final Buffer * name = ad_buffer_from_ascii("ECU random");
+            ad_buffer_append_byte(payload, infoType);
+            if ( generator->flavour.is_Iso15765_4 ) {
+                int number_of_data_items = 1;
+                ad_buffer_append_byte(payload, number_of_data_items);                
+            }
+            ad_buffer_padding(name, 20, 0x00);
+            ad_buffer_append_melt(payload, name);
+        } break;
+    }
+
+    Buffer * binResponse = ad_buffer_new();
+    if ( generator->flavour.is_Iso15765_4 ) {
+        ad_buffer_append_byte(binResponse, OBD_SERVICE_REQUEST_VEHICLE_INFORMATION | OBD_DIAGNOSTIC_SERVICE_POSITIVE_RESPONSE);
+        ad_buffer_append(binResponse, payload);
+    } else {
+        // split message in multiple frames if not Iso15765-4
+        if ( (infoType % 2) == 0 ) {
+            for(int message_in = 1; message_in < payload->size; message_in+=4) {
+                ad_buffer_append_byte(binResponse, OBD_SERVICE_REQUEST_VEHICLE_INFORMATION | OBD_DIAGNOSTIC_SERVICE_POSITIVE_RESPONSE);
+                ad_buffer_append_byte(binResponse, infoType);
+                ad_buffer_append_byte(binResponse, message_in);
+                ad_buffer_slice_append(binResponse, payload, message_in, 4);
+            }
+        } else {
+            ad_buffer_append_byte(binResponse, OBD_SERVICE_REQUEST_VEHICLE_INFORMATION | OBD_DIAGNOSTIC_SERVICE_POSITIVE_RESPONSE);
+            ad_buffer_append_byte(binResponse, infoType);
+            ad_buffer_append_melt(binResponse, payload);
+        }
+    }
+    return binResponse;
+}
 static Buffer * response(SimECUGenerator *generator, final Buffer *binRequest) {
     assert(generator->context != null);
     unsigned * seed = generator->context;
@@ -50,69 +175,8 @@ static Buffer * response(SimECUGenerator *generator, final Buffer *binRequest) {
             log_msg(LOG_DEBUG, "Clearing DTCs");
         } break;
 
-        case OBD_SERVICE_REQUEST_VEHICLE_INFORMATION: {
-            if ( 1 < binRequest->size ) {
-                switch(binRequest->buffer[1]) {
-                    case 0x00: {
-                        ad_buffer_append_melt(binResponse, ad_buffer_from_ascii_hex("FFFFFFFFFF"));
-                        break;
-                    }
-                    case 0x01: {
-                        ad_buffer_append_byte(binResponse, 0x05);
-                        break;
-                    }
-                    case OBD_SERVICE_REQUEST_VEHICLE_INFORMATION_VIN: {
-                        ad_buffer_append_melt(binResponse,
-                            ad_buffer_new_random_with_seed(17, seed));
-                        break;
-                    }
-                    case 0x03: {
-                        ad_buffer_append_byte(binResponse,0x01);
-                        break;
-                    }
-                    case 0x04: {
-                        ad_buffer_append_melt(binResponse,
-                            ad_buffer_new_random_with_seed(16, seed));
-                        break;
-                    }
-                    case 0x05: {
-                        ad_buffer_append_byte(binResponse,0x01);
-                        break;
-                    }
-                    case 0x06: {
-                        ad_buffer_append_melt(binResponse,
-                            ad_buffer_new_random_with_seed(4, seed));
-                        break;
-                    }
-                    case 0x07: {
-                        ad_buffer_append_byte(binResponse,0x01);
-                        break;
-                    }
-                    case 0x08: {
-                        ad_buffer_append_melt(binResponse,
-                            ad_buffer_new_random_with_seed(4, seed));
-                        break;
-                    }
-                    case 0x09: {
-                        ad_buffer_append_byte(binResponse,0x01);
-                        break;
-                    }
-                    case OBD_SERVICE_REQUEST_VEHICLE_INFORMATION_ECU_NAME: {
-                        final Buffer * name = ad_buffer_from_ascii("ECU random");
-                        ad_buffer_padding(name, 20, 0x00);
-                        ad_buffer_append_melt(binResponse, name);
-                        break;
-                    }
-                    case 0x0B: {
-                        ad_buffer_append_melt(binResponse,
-                            ad_buffer_new_random_with_seed(4, seed));
-                        break;
-                    }
-                }
-            } else {
-                log_msg(LOG_ERROR, "Missing PID");
-            }
-        } break;
+        case OBD_SERVICE_REQUEST_VEHICLE_INFORMATION:
+            return response_saej1979_vehicle_identification_request(generator, binRequest);        
     }
     return binResponse;
 }
