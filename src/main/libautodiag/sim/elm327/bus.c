f300335294a04037bb19f188b24f42db5be75e8a
autodiag2 <autodiag@netcourrier.com>
2026-08-09 15:12:54 +0200

    incorrect length

--- src/main/libautodiag/sim/elm327/bus.c
+++ src/main/libautodiag/sim/elm327/bus.c
@@ -75,7 +75,7 @@ static ad_list_Buffer * request_frames(SimELM327* elm327, SimECU * ecu, Buffer *
             if ( AD_ISO15765_SINGLE_FRAME_MAX_BYTES < dataRequest->size ) {
                 int data_length = dataRequest->size;
                 ad_buffer_append_byte(one_frame, (Iso15765FirstFrame << 4) | ((data_length >> 8) & 0xF));
-                ad_buffer_append_byte(one_frame, data_length & 0xF);
+                ad_buffer_append_byte(one_frame, data_length & 0xFF);
                 ad_buffer_slice_append(one_frame, dataRequest, 0, AD_ISO15765_FIRST_FRAME_MAX_BYTES);
                 for(int i = AD_ISO15765_FIRST_FRAME_MAX_BYTES, sn = 1; i < dataRequest->size; i += AD_ISO15765_CONSECUTIVE_FRAME_MAX_BYTES, sn ++) {
                     Buffer * consecutive_frame = ad_buffer_new();
